0c73fd918af9dfc9320be3bcb17f4aae27e99eb9
Midousuji970 <famextreme970@gmail.com>
2022-05-07 11:16:44 -0300

    final v2

--- painel.php
+++ painel.php
@@ -1,12 +1,29 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Autobalance</title>
-</head>
-<body>
-
-</body>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8">
+        <title>
+            Auto Balance
+        </title>
+        <link rel="stylesheet" type="text/css" href="css/main.css?=v5">
+    </head>
+    <body>
+    <header>
+            <a href="index.html"><h1 class="h1-nav">Auto Balance</h1></a>
+            <nav>
+                <ul class="nav_links">
+                    <li><a href='index.html'>Home</a></li>
+                    <li><a href="quem-somos.html">Quem Somos</a></li>
+                    <li><a href="requisicao.html">Requisição</a></li>
+                    <li><a href="login.php">Login</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <h1 class="textcenter">Pagina em Manutenção!</h1>
+            <a href="index.html" class="textcenter"><h1><br>Voltar para o Inicio</h1></a>
+        </main>
+    </body>
 </html>
